2f22078c8e1031ef7bf4ebca13acdd99263424fb
joshstseng <joshuastseng@gmail.com>
2022-12-10 18:39:27 -0500

    able to make program open a url

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.awt.*;
+import java.net.URI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("hello");
+
+        try {
+            URI test = new URI("https://stackoverflow.com/questions/10967451/open-a-link-in-browser-with-java-button");
+            Video.openWebpage(test);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/Video.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.net.URI;
+
+public class Video {
+
+    public static boolean openWebpage(URI uri) {
+        Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            try {
+                desktop.browse(uri);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+}
