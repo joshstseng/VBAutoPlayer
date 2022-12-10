eee0eb4b3cd2cd06426b4dfbac06ac4c64fdd891
joshstseng <joshuastseng@gmail.com>
2022-12-10 18:59:45 -0500

    started date class started video class

---
+++ src/Date.java
@@ -0,0 +1,70 @@
+/*
+    class Date uses military time for easier comparisons
+    between videos
+ */
+
+public class Date {
+
+    int month;
+    int day;
+    int year;
+    int hour;
+    int minute;
+
+    public Date() {
+        month = 1;
+        day = 1;
+        year = 2000;
+        hour = 0;
+        minute = 0;
+    }
+
+    public Date(int month, int day, int year, int hour, int minute) {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+}
--- src/Video.java
+++ src/Video.java
@@ -1,8 +1,39 @@
 import java.awt.*;
 import java.net.URI;
+//import java.util.Date;
 
 public class Video {
 
+    URI uri;
+    Date datePosted;
+
+    public Video(URI uri, Date datePosted) {
+        this.uri = uri;
+        this.datePosted = datePosted;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    /*
+    * opens webpage with a given uri
+    * uses desktops default browser
+    * exceptions are caught and stack trace is printed
+    * returns true on success
+    * */
     public static boolean openWebpage(URI uri) {
         Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
         if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
