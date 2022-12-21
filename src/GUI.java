a2127399f8e22ad791d52a2489d63d5a0a1a3187
joshstseng <joshuastseng@gmail.com>
2022-12-21 10:42:00 -0800

    clearButton works

--- src/GUI.java
+++ src/GUI.java
@@ -88,6 +88,7 @@ public class GUI implements ActionListener {
                     }
                     runner.clearPlaylist();
                     labels.clear();
+                    showFrame();
                 }
             });
 
